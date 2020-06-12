02adb180d95a941e5fd46cf78ba193327bcaaeca
Katser <gray84@tut.by>
2020-06-12 18:38:49 +0300

    Исправили предыдущий коммит

--- js/main.mjs
+++ js/main.mjs
@@ -1,3 +1,21 @@
 window.onload = () => {
 	console.log("Страница загружена")
+	const _navListener = ()=> {
+		const burger = document.getElementById('burger')
+		const navigation = document.querySelector('.navigation')
+		const navClose = document.getElementById('nav-close');
+		burger.addEventListener('click',(e)=>{
+			e.preventDefault()
+			console.log(1)
+			navigation.classList.toggle('active')
+		})
+		navClose.addEventListener('click',(e)=>{
+			e.preventDefault()
+			navigation.classList.remove('active')
+		})
+	}
+	const _init = () =>{
+		_navListener()
+	}
+	_init()
 }
