e0898283b35d947d54ddbef95b7a589010632506
Katser <gray84@tut.by>
2020-06-13 23:18:24 +0300

    Сделана часть Preview

--- templates/default/script.js
+++ templates/default/script.js
@@ -1,21 +1,39 @@
 window.onload = () => {
 	console.log("Страница загружена")
-	const _navListener = ()=> {
+	const _navListener = () => {
 		const burger = document.getElementById('burger')
 		const navigation = document.querySelector('.navigation')
 		const navClose = document.getElementById('nav-close');
-		burger.addEventListener('click',(e)=>{
+		burger.addEventListener('click', (e) => {
 			e.preventDefault()
 			console.log(1)
 			navigation.classList.toggle('active')
 		})
-		navClose.addEventListener('click',(e)=>{
+		navClose.addEventListener('click', (e) => {
 			e.preventDefault()
 			navigation.classList.remove('active')
 		})
 	}
-	const _init = () =>{
+	const _previewSlider = () => {
+		$('.previewSlick').slick({
+			nextArrow: "<div class='slick-next'><img src='./templates/default/images/slider_arrow.png' alt='next'></div>",
+			prevArrow: "<div class='slick-prev'><img src='./templates/default/images/slider_arrow.png' alt='prev'></div>",
+			dots: true
+		});
+	}
+	const _activatePreviewMore = ()=>{
+		const previewMore = document.querySelector('.previewFree__more')
+		previewMore.addEventListener('click',(e)=>{
+			e.preventDefault()
+			console.log(1)
+			previewMore.classList.toggle('active')
+		})
+	}
+	
+	const _init = () => {
 		_navListener()
+		_previewSlider()
+		_activatePreviewMore()
 	}
 	_init()
 }
