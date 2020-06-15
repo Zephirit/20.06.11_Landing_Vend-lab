3b7d4e2f450e2444929135094344f01a870a2a64
Katser <gray84@tut.by>
2020-06-15 17:01:46 +0300

    Сделали кучу правок

--- templates/default/script.js
+++ templates/default/script.js
@@ -3,24 +3,32 @@ window.onload = () => {
 		const burger = document.getElementById('burger')
 		const navigation = document.querySelector('.navigation')
 		const navClose = document.getElementById('nav-close');
+		const body = document.querySelector('.wrapper')
 		burger.addEventListener('click', (e) => {
 			e.preventDefault()
 			navigation.classList.toggle('active')
 			burger.classList.toggle('active')
+			document.body.classList.toggle('notScroll')
 		})
 		navClose.addEventListener('click', (e) => {
 			e.preventDefault()
 			navigation.classList.remove('active')
+			burger.classList.remove('active')
+			document.body.classList.remove('notScroll')
 		})
 	}
 	const _previewSlider = () => {
-		$('.previewSlick').slick({
+		const slick = $('.previewSlick').slick({
 			nextArrow: "<div class='slick-next'><img src='./templates/default/images/slider_arrow.png' alt='next'></div>",
 			prevArrow: "<div class='slick-prev'><img src='./templates/default/images/slider_arrow.png' alt='prev'></div>",
 			dots: true,
-			fade: true
-			
-		});
+			fade: true,
+			autoplay: true,
+			autoplaySpeed: 5000,
+		})
+		// slick.on('beforeChange', (slick) => {
+		// 	console.log(slick)
+		// })
 	}
 	const _reviewsSlider = () => {
 		$('.reviewsSlick').slick({
@@ -28,7 +36,7 @@ window.onload = () => {
 			prevArrow: "<div class='slick-prev'><img src='./templates/default/images/slider_arrow.png' alt='prev'></div>",
 			dots: true,
 			autoplay: true,
-			autoplaySpeed: 3000
+			autoplaySpeed: 5000
 		});
 		const dots = document.querySelector('.reviewsSlick .slick-dots')
 		const dotItems = dots.querySelectorAll('li')
@@ -38,11 +46,28 @@ window.onload = () => {
 		});
 	}
 	const _newsSlider = () => {
-		if (window.innerWidth < 768) {
-			$('.news__items').slick({
-				dots: true
-			})
-		}
+		$('.news__items').slick({
+			dots: true,
+			slidesToShow: 1,
+			infinite: true,
+			mobileFirst: true,
+			responsive: [{
+					breakpoint: 1199,
+					settings: {
+						slidesToShow: 4,
+						dots: false,
+					}
+				},
+				{
+					breakpoint: 767,
+					settings: {
+						slidesToShow: 3,
+						dots: false,
+					}
+				}
+			]
+
+		})
 	}
 	const _partnersSlider = () => {
 		$('.partners__items').slick({
@@ -54,6 +79,8 @@ window.onload = () => {
 			centerPadding: '10px',
 			slidesToShow: 1,
 			mobileFirst: true,
+			autoplay: true,
+			autoplaySpeed: 3000,
 			responsive: [{
 					breakpoint: 1200,
 					settings: {
@@ -89,6 +116,46 @@ window.onload = () => {
 		})
 	}
 
+	function _popup() {
+		const popup = document.querySelector('#popup');
+		const _popupListeners = () => {
+			const triggers = document.querySelectorAll('.popupTrg');
+			triggers.forEach(trg => {
+				trg.addEventListener('click', e => {
+					e.preventDefault();
+					popup.classList.add('popup--active');
+					popup.classList.remove('popup--closed');
+					document.body.classList.toggle('notScroll')
+				});
+			});
+		};
+
+		const _destroyListener = () => {
+			const closeBtn = popup.querySelector('.popup__close');
+			const blurred = document.querySelector('.popup__bg');
+			console.log(blurred)
+			closeBtn.addEventListener('click', e => {
+				e.preventDefault();
+				popup.classList.remove('popup--active');
+				popup.classList.add('popup--closed');
+				document.body.classList.remove('notScroll')
+			});
+			blurred.addEventListener('click', e => {
+				e.preventDefault();
+				console.log(1)
+				popup.classList.remove('popup--active');
+				popup.classList.add('popup--closed');
+				document.body.classList.remove('notScroll')
+			})
+
+		};
+
+		const _init = () => {
+			_popupListeners();
+			_destroyListener();
+		};
+		_init();
+	}
 	const _init = () => {
 		_navListener()
 		_previewSlider()
@@ -96,6 +163,7 @@ window.onload = () => {
 		_newsSlider()
 		_partnersSlider()
 		_activatePreviewMore()
+		_popup()
 	}
 	_init()
 }
